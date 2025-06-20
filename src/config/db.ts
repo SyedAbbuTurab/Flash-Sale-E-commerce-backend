faf7b367d5b9af1e6a7995f1224a4756bb82c36a
Abbu Turab <syedabbuturab786@gmail.com>
2025-06-19 23:55:12 -0500

    updated coneection pooling for DB

--- src/config/db.ts
+++ src/config/db.ts
@@ -10,7 +10,7 @@ export const sequelize = new Sequelize(
     dialect: 'postgres',
     logging: false,
     pool: {
-      max: 20,     // increase from default 5
+      max: 50,     // increase from default 5
       min: 0,
       acquire: 30000,
       idle: 10000
