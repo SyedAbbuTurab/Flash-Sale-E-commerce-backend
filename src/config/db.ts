4eb341d074b439ff0d75b3ff9be3260e15f4e389
Abbu Turab <syedabbuturab786@gmail.com>
2025-06-24 16:50:29 -0500

    Remove emoji's

--- src/config/db.ts
+++ src/config/db.ts
@@ -23,9 +23,9 @@ console.log('Password from env:', process.env.DB_PASS);
 export const testDbConnection = async () => {
   try {
     await sequelize.authenticate();
-    console.log('✅ Database connected successfully.');
+    console.log('Database connected successfully.');
   } catch (error) {
-    console.error('❌ Unable to connect to the database:', error);
+    console.error('Unable to connect to the database:', error);
     process.exit(1);
   }
 };
