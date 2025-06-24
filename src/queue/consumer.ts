984604e7c95b0372fbc70f302c70bdf817d31e4e
Abbu Turab <syedabbuturab786@gmail.com>
2025-06-24 16:51:05 -0500

    Remove emoji's

--- src/queue/consumer.ts
+++ src/queue/consumer.ts
@@ -8,17 +8,17 @@ export const startConsumer = async () => {
       const data = await redis.blpop('purchaseQueue', 0);
 
       if (!data || data.length < 2) {
-        console.warn('⚠️ No data received from queue');
+        console.warn('No data received from queue');
         continue;
       }
 
       const [, payload] = data;
-      console.log('🔄 Pulled from queue:', payload);
+      console.log('Pulled from queue:', payload);
 
       const { userId, productId } = JSON.parse(payload);
       await processPurchase(userId, productId);
     } catch (error) {
-      console.error('❌ Consumer crashed with error:', error);
+      console.error('Consumer crashed with error:', error);
     }
   }
 };
