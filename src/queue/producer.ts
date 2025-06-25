efe4ccb2e17a99af05da4c4b1064961574432884
Abbu Turab <syedabbuturab786@gmail.com>
2025-06-25 16:24:12 -0500

    added timers to controll the pull from queue

--- src/queue/producer.ts
+++ src/queue/producer.ts
@@ -1,13 +1,12 @@
 import redis from '../utils/redisClient';
 
+
 export const queuePurchaseRequest = async (payload: any) => {
   try {
     console.log('📝 Enqueueing into Redis:', payload);
-    const result= await redis.lpushx('purchaseQueue', JSON.stringify(payload));
+    const result= await redis.lpush('purchaseQueue', JSON.stringify(payload));
     console.log('✅ Enqueued successfully', {result});
   } catch (err) {
     console.error('❌ Failed to enqueue:', err);
-  } finally {
-    redis.disconnect(); 
   }
 };
