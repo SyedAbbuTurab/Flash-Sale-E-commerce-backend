99469a279ca9544ca9b4eb76b14720ac1275c550
Abbu Turab <syedabbuturab786@gmail.com>
2025-06-25 14:31:20 -0500

    Added loggers to debug

--- src/queue/consumer.ts
+++ src/queue/consumer.ts
@@ -2,23 +2,23 @@ import redis from '../utils/redisClient';
 import { processPurchase } from '../services/purchaseService';
 
 export const startConsumer = async () => {
+  console.log('🔄 Consumer started...');
   while (true) {
+    const data = await redis.blpop('purchaseQueue', 0);
+    
+    if (!data || data.length < 2) {
+      console.warn('⚠️ No data received from queue');
+      continue;
+    }
+
+    const [, payload] = data;
+    console.log("📦 Processing payload:", payload);
+
     try {
-      console.log('⏳ Waiting for queue item...');
-      const data = await redis.blpop('purchaseQueue', 0);
-
-      if (!data || data.length < 2) {
-        console.warn('No data received from queue');
-        continue;
-      }
-
-      const [, payload] = data;
-      console.log('Pulled from queue:', payload);
-
       const { userId, productId } = JSON.parse(payload);
       await processPurchase(userId, productId);
     } catch (error) {
-      console.error('Consumer crashed with error:', error);
+      console.error('❌ Error processing purchase:', error);
     }
   }
 };
