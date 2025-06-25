9056d135338150bd7ddf9babe707ebf131c2005b
Abbu Turab <syedabbuturab786@gmail.com>
2025-06-25 16:23:57 -0500

    added timers to controll the pull from queue

--- src/queue/consumer.ts
+++ src/queue/consumer.ts
@@ -3,22 +3,25 @@ import { processPurchase } from '../services/purchaseService';
 
 export const startConsumer = async () => {
   console.log('🔄 Consumer started...');
-  while (true) {
-    const data = await redis.blpop('purchaseQueue', 0);
-    
-    if (!data || data.length < 2) {
-      console.warn('⚠️ No data received from queue');
-      continue;
-    }
 
-    const [, payload] = data;
-    console.log("📦 Processing payload:", payload);
-
+  const consume = async () => {
     try {
-      const { userId, productId } = JSON.parse(payload);
-      await processPurchase(userId, productId);
-    } catch (error) {
-      console.error('❌ Error processing purchase:', error);
+      console.log("🕐 Waiting for Redis item...");
+      const data = await redis.blpop('purchaseQueue', 5); // wait max 5s
+      if (data && data.length >= 2) {
+        const [, payload] = data;
+        console.log("🔄 Pulled from queue:", payload);
+        const { userId, productId } = JSON.parse(payload);
+        await processPurchase(userId, productId);
+      } else {
+        console.log("⏳ No new data in Redis queue");
+      }
+    } catch (err) {
+      console.error('❌ Error in consumer loop:', err);
     }
-  }
+
+    setTimeout(consume, 100); // keep polling
+  };
+
+  consume(); // start
 };
